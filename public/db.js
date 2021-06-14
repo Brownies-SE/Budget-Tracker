334efedb0ada5f8acb04855c25bff535a3f61183
Josh Brown <brownjoshalan@gmail.com>
2021-06-14 17:40:43 -0600

    Created the checkDatabase function

--- public/db.js
+++ public/db.js
@@ -21,10 +21,33 @@ request.onerror = function (event) {
 
 function saveRecord(record) {
     const transaction = db.transaction(["pending", "readwrite"]),
-    const store = trasnaction.ObjectStore("pending")
+    const store = transaction.ObjectStore("pending")
     store.add(record)
 }
 
-function checkDatabase() {}
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite") 
+    const store = transaction.ObjectStore("pending")
+    const getAll = store.getAll()
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json",
+                }
+            })
+            .then((response) => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.ObjectStore("pending");
+                store.clear();
+            })
+        }
+    }
+}
 
 window.addEventListener("online", checkDatabase);
