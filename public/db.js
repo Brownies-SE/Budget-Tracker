6200d1e5b7694532e1a226fe52e2bb2fccf1434d
Josh Brown <brownjoshalan@gmail.com>
2021-06-14 20:45:07 -0600

    Fixed multiple bugs and mispelling, offline works correctly

--- public/db.js
+++ public/db.js
@@ -1,17 +1,15 @@
-const { request } = require("express");
-
 let db;
+const request = indexedDB.open("Budget", 1);
 
 request.onupgradeneeded = function (event) {
-  const db = event.target.result;
-  db.createObjectStore("BudgetStore", { autoIncrement: true });
+  db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
   db = event.target.result;
-
   if (navigator.onLine) {
-    checkDatabase;
+    checkDatabase();
   }
 };
 
@@ -20,34 +18,34 @@ request.onerror = function (event) {
 };
 
 function saveRecord(record) {
-    const transaction = db.transaction(["pending", "readwrite"]),
-    const store = transaction.ObjectStore("pending")
-    store.add(record)
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
 }
 
 function checkDatabase() {
-    const transaction = db.transaction(["pending"], "readwrite") 
-    const store = transaction.ObjectStore("pending")
-    const getAll = store.getAll()
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
 
-    getAll.onsuccess = function () {
-        if (getAll.result.length > 0) {
-            fetch("/api/transaction/bulk", {
-                method: "POST",
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: "application/json, text/plain, */*",
-                    "Content-Type": "application/json",
-                }
-            })
-            .then((response) => response.json())
-            .then(() => {
-                const transaction = db.transaction(["pending"], "readwrite");
-                const store = transaction.ObjectStore("pending");
-                store.clear();
-            })
-        }
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
     }
+  };
 }
 
 window.addEventListener("online", checkDatabase);
